e57f1927298216d30755cb3d7e13037414a102ec
nafeesa.arif <nafeesa.arif@TS-NAFEESA-PC>
2023-07-31 20:10:14 +0500

    Constant File implementation

---
+++ BookAPI/Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace BookAPI.Data
+{
+    public static class Constants
+    {
+        public const string NotFound = "Error... NotFound";
+        public const string Deletion = "Deleted Successfully";
+        public const string Updation = "Updated Successfully";
+    }
+}
--- BookAPI/Controllers/AuthorController.cs
+++ BookAPI/Controllers/AuthorController.cs
@@ -33,7 +33,7 @@ namespace BookAPI.Controllers
             var author = await _context.Author.FirstOrDefaultAsync(a=>a.Name==name);
             if (author == null) 
             {
-                return NotFound("Not Found");
+                return NotFound(Constants.NotFound);
             }
 
             return Ok(author);
@@ -85,82 +85,13 @@ namespace BookAPI.Controllers
             {
                 _context.Remove(contact);
                 await _context.SaveChangesAsync();
-                return Ok("Deleted successfully");
+                return Ok(Constants.Deletion);
             }
 
             return NotFound();
 
 
         }
-        /*
-            // POST: Author/Edit/5
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Gender")] AuthorModel authorModel)
-            {
-                if (id != authorModel.Id)
-                {
-                    return NotFound();
-                }
-
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        _context.Update(authorModel);
-                        await _context.SaveChangesAsync();
-                    }
-                    catch (DbUpdateConcurrencyException)
-                    {
-                        if (!AuthorModelExists(authorModel.Id))
-                        {
-                            return NotFound();
-                        }
-                        else
-                        {
-                            throw;
-                        }
-                    }
-                    return RedirectToAction(nameof(Index));
-                }
-                return View(authorModel);
-            }
-
-            // GET: Author/Delete/5
-            public async Task<IActionResult> Delete(Guid? id)
-            {
-                if (id == null || _context.Author == null)
-                {
-                    return NotFound();
-                }
-
-                var authorModel = await _context.Author
-                    .FirstOrDefaultAsync(m => m.Id == id);
-                if (authorModel == null)
-                {
-                    return NotFound();
-                }
-
-                return View(authorModel);
-            }
-            // POST: Author/Delete/5
-            [HttpPost, ActionName("Delete")]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> DeleteConfirmed(Guid id)
-            {
-
-                var authorModel = await _context.Author.FindAsync(id);
-                if (authorModel != null)
-                {
-                    _context.Author.Remove(authorModel);
-                }
-
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            private bool AuthorModelExists(Guid id)
-            {
-                return (_context.Author?.Any(e => e.Id == id)).GetValueOrDefault();
-            }*/
+       
     }
 }
