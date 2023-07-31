86170e82029e68eed1822019512c0d1d402877d4
nafeesa.arif <nafeesa.arif@TS-NAFEESA-PC>
2023-07-31 15:34:03 +0500

    Initial Project Structure

---
+++ BookAPI/Data/BookAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Data
+{
+    public class BookAPIContext : DbContext
+    {
+
+
+        public BookAPIContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BookModel> Books { get; set; }
+        public DbSet<AuthorModel> Author { get; set; }
+    }
+
+}
+
+
